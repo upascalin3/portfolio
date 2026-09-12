eca0692812998f903ea5c2a4c4155dbb35730a96
upascalin3 <uwayopascaline87@gmail.com>
2026-09-12 17:42:11 -0400

    skill update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -319,7 +319,7 @@ export default function Home() {
           <div className="mt-12">
             <Expandable
               items={skillGroups}
-              previewCount={3}
+              previewCount={4}
               moreLabel="More skills"
               className="grid gap-10 md:grid-cols-2 lg:grid-cols-3"
               render={([group, items]) => (
